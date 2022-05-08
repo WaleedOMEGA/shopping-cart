35dbc6c86d8fb9126d107b6aa89f10194b4e7841
waleedomega <waleedomega@hotmail.com>
2022-05-08 11:01:14 +0200

    install react-modal

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../../css/Products/Products.css';
+import Modal from 'react-modal';
 function Products(props){
-	return (<div className="products-wrapper">{props.products.map(product => (
-		<div key={product.id} className="product-item">
-			<img src={product.imageUrl} alt={product.title} />
+	return (
+		<div className="products-wrapper">
+			{props.products.map((product) => (
+				<div key={product.id} className="product-item">
+					<a href="#">
+						<img src={product.imageUrl} alt={product.title} />
+					</a>
 
-			<div className="product-desc">
-				<p>{product.title}</p>
-				<span>{product.price}</span>
-			</div>
-			<button>Add to Cart</button>
+					<div className="product-desc">
+						<p>{product.title}</p>
+						<span>{product.price}</span>
+					</div>
+					<button>Add to Cart</button>
+				</div>
+			))}
 		</div>
-		
-	))}</div>
 	);
 }
 
