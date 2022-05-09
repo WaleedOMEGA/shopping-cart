f7753e6d1597486bfdc908f62795d2ac35d09cdc
WALEEDSALAH-PC\Professional <waleedomega@hotmail.com>
2022-05-09 16:49:54 +0200

    filter component init

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 // import { words } from './words.js';
 import data from './data.json';
 import Products from './components/Products/Products';
+import Filter from './components/Filter/Filter';
 function App() {
   const [products,setProducts] = useState(data);
   return (
@@ -13,7 +14,7 @@ function App() {
 			<main>
         <div className="wrapper">
           <Products products={products}/>
-					<div className='filter-wrapper'>Filter</div>
+				<Filter/>	
 				</div>
 			</main>
 			<Footer />
